3f71085759069d6fa3e7016fea4bd5864543ec78
Katina Ferterbest <kferterb@er-c3.kzn.21-school.ru>
2022-03-19 14:14:18 +0300

    exam03

---
+++ mini_paint.c
@@ -0,0 +1,52 @@
+#include <math.h>
+#include <unistd.h>
+#include <stdio.h>
+int ft_close(FILE *file) {
+	write(1, "Error: Operation file corrupted\n", 32);
+	fclose(file);
+	return 1;
+}
+int main(int argc, char **argv) {
+	FILE *file;
+	int i = -1, j, height, width, arg;
+	float x, y, radius, res;
+	char back, c, symb;
+	if(argc != 2)
+		return(write(1, "Error: argument\n", 16) && 1);
+	if(!(file = fopen(argv[1], "r")))
+		return(write(1, "Error: Operation file corrupted\n", 32) && 1);
+	if((arg = fscanf(file, "%d %d %c\n", &width, &height, &back)) != 3)
+		return(ft_close(file));
+	if(width <= 0 || width > 300 || height <= 0 || height > 300)
+		return(ft_close(file));
+	char arr[height][width];
+	while(++i < height) {
+		j = -1;
+		while(++j < width)
+			arr[i][j] = back;
+	}
+	while((arg = fscanf(file, "%c %f %f %f %c\n", &c, &x, &y, &radius, &symb)) == 5) {
+		if((c != 'c' && c != 'C') || radius <= 0)
+			return(ft_close(file));
+		i = -1;
+		while(++i < height) {
+			j = -1;
+			while(++j < width) {
+				res = sqrt(pow(i - y, 2) + pow(j - x, 2));
+				if(res <= radius)
+					if((radius - res < 1 && c == 'c') || c == 'C')
+						arr[i][j] = symb;
+			}
+		}
+	}
+	if(arg > 0 && arg != 5)
+		return(ft_close(file));
+	i = -1;
+	while(++i < height) {
+		j = -1;
+		while(++j < width)
+			write(1, &arr[i][j], 1);
+		write(1, "\n", 1);
+	}
+	return(fclose(file) && 0);
+}
